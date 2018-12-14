c1078c5fc52be4e5488f86e7601f73f823784d91
Daniel Kloeters <dkloeters@hax-solutions.de>
2018-12-14 12:42:10 +0100

    raised micro version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Generate profile pages / data-sheets based on simpl-schema",
-    version: "0.0.17",
+    version: "0.0.18",
     name: "d3k4y:meteor-autoprofile",
     git: "https://github.com/d3k4y/meteor-autoprofile"
 });
